6cbba70fd4c6da70c98527406910da0dee74edf0
GreatOne33 <GreatOne33@users.noreply.github.com>
2026-06-18 18:38:21 -0400

    Adding Default Cert TLS checkov bypass as this is a lab

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -280,6 +280,7 @@ resource "aws_cloudfront_distribution" "cicd_website_distribution" {
     }
   }
 
+  # checkov:skip=CKV_AWS_174: Using default CloudFront cert for lab training; ACM/Custom domain not attached yet.
   viewer_certificate {
     cloudfront_default_certificate = true
     minimum_protocol_version = "TLSv1.2_2021"
